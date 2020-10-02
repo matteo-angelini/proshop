ff0b6b4edbf4b21fd4e3aaca51c5b0b3f0949aff
matteo-angelini <matteo.angelini@outlook.com>
2020-10-02 17:36:41 +0200

    FrontEnd config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className='app'>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
