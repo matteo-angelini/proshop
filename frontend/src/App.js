087ed8068e5f5a67d9daa77b9552ad65ccf9c3a8
matteo-angelini <matteo.angelini@outlook.com>
2020-10-02 17:57:09 +0200

    Product Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Proshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
