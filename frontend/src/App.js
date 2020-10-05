9217ef4f738143cbb6b743a87fd6a91e825ee21b
matteo-angelini <matteo.angelini@outlook.com>
2020-10-05 17:01:47 +0200

    Nodemon & Concurrently

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { Container } from 'react-bootstrap';
