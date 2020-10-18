34a345cab3ab611d1851f26c92781b8593a79bdd
matteo-angelini <matteo.angelini@outlook.com>
2020-10-18 14:42:44 +0200

    Product Routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express'
+const router = express.Router();
+
+import Product from '../models/productModel.js'
+import AsyncHandler from 'express-async-handler'
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access public
+
+router.get('/', AsyncHandler( async (req, res) => {
+  const products = await Product.find({})
+
+  res.json(products);
+}));
+
+// @desc Fetch single products
+// @route GET /api/products/:id
+// @access public
+
+router.get('/:id',AsyncHandler(async (req, res) => {
+  const products = await Product.findById(req.params.id);
+
+  if(products){
+    res.json(products)
+  } else{
+    res.status(404).json({message: 'product not found'})
+  }
+}));
+
+export default router;
