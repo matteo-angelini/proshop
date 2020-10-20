cabe95fbceef20128bf463a7747dd9b2420d9f73
matteo-angelini <matteo.angelini@outlook.com>
2020-10-20 17:32:48 +0200

    Loader and Message Components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,37 @@
-import express from 'express'
+import express from 'express';
 const router = express.Router();
 
-import Product from '../models/productModel.js'
-import AsyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js';
+import AsyncHandler from 'express-async-handler';
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access public
 
-router.get('/', AsyncHandler( async (req, res) => {
-  const products = await Product.find({})
-
-  res.json(products);
-}));
+router.get(
+  '/',
+  AsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc Fetch single products
 // @route GET /api/products/:id
 // @access public
 
-router.get('/:id',AsyncHandler(async (req, res) => {
-  const products = await Product.findById(req.params.id);
+router.get(
+  '/:id',
+  AsyncHandler(async (req, res) => {
+    const products = await Product.findById(req.params.id);
 
-  if(products){
-    res.json(products)
-  } else{
-    res.status(404)
-    throw new Error(`Product not found`)
-  }
-}));
+    if (products) {
+      res.json(products);
+    } else {
+      res.status(404);
+      throw new Error(`Product not found`);
+    }
+  })
+);
 
 export default router;
